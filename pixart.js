e6ff6f19c2be2ae3582f321248d6ec1f978622db
darryl <darrylcohen@iinet.net.au>
2017-10-20 15:35:26 +1100

    step 7 very ugly

--- pixart.js
+++ pixart.js
@@ -12,12 +12,53 @@ $("#color-field").keydown(function (e) {
   }
 });
 
-$colour = $('.brush').css("background-color")
-for(var i = 0; i < 1000; i++) {
-  var $square = $('<div>')
-  $square.addClass('square')
-  $square.on('mouseover', function(event){
-    $(event.target).css("background-color", $colour);
-  })
-  $('body').append($square)
+var doColour = function() {
+  $colour = $('.brush').css("background-color")
+  for(var i = 0; i < 1000; i++) {
+    var $square = $('<div>')
+    $square.addClass('square')
+    $square.on('click', function(event){
+      $(event.target).css("background-color", $colour);
+    })
+    $('body').append($square)
+  }
 }
+
+var imageString = '';
+$('.movieBTN').on('click', function(){
+
+  $.ajax({
+        url: 'http://omdbapi.com/?',
+          method: 'get',
+          data: {
+            t: 'Jaws',
+            apikey: '2f6435d9'
+          }
+      }).done(function(resp) {
+        imageString = resp.Poster
+      })
+})
+
+
+var doStamp = function() {
+
+  for(var i = 0; i < 1000; i++) {
+    // var $square = $('<div>')
+    var $image = $('<img>')
+    // $square.append($image)
+    $image.addClass('square')
+    $image.on('click', function(event){
+
+      $(event.target).attr("src", imageString);
+
+    })
+    $('body').append($image)
+  }
+}
+
+$('.doColourBTN').on('click', function(){
+  doColour()
+})
+$('.doMovieBTN').on('click', function(){
+  doStamp()
+})

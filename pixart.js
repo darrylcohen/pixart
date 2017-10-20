68142f11a9dfc19471c5642b0f2d23e80b29ed2b
darryl <darrylcohen@iinet.net.au>
2017-10-20 14:34:45 +1100

    step 2

--- pixart.js
+++ pixart.js
@@ -3,3 +3,11 @@ $('#set-color').on('click', function(event){
   $colour = $('#color-field').val()
   $('.brush').css("background-color", $colour);
 })
+
+$("#color-field").keydown(function (e) {
+  if (e.keyCode == 13) {
+    event.preventDefault()
+    $colour = $('#color-field').val()
+    $('.brush').css("background-color", $colour);
+  }
+});
